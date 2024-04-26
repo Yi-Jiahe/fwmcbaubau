b5c19f472187ba3d7cc476df3eb2a725cdfc63d2
Jiahe <yi.jiahe00@gmail.com>
2024-04-26 23:39:07 +0800

    Merge multiple stream status
    Squashed commit of the following:
    
    commit 9184efc78975f47744b9cc8bef2fc63bc153178b
    Author: Jiahe <yi.jiahe00@gmail.com>
    Date:   Fri Apr 26 22:52:39 2024 +0800
    
        Revert interval
    
    commit 2b453b4c6214d9599d516c2d128e2271a832f39c
    Author: Jiahe <yi.jiahe00@gmail.com>
    Date:   Fri Apr 26 22:49:32 2024 +0800
    
        Add pointer to buttons
    
    commit a6ad359dbcb4af5b8791f9cc82f7fb63e4fa9027
    Author: Jiahe <yi.jiahe00@gmail.com>
    Date:   Fri Apr 26 22:47:09 2024 +0800
    
        Make buttons taller
    
    commit 783aa548f8e13e77b63715958949cfd9fee30a9b
    Author: Jiahe <yi.jiahe00@gmail.com>
    Date:   Fri Apr 26 22:26:50 2024 +0800
    
        Adjust sizes
    
    commit c3ff6e25cf923f174c9e62357cd16715568e9868
    Author: Jiahe <yi.jiahe00@gmail.com>
    Date:   Fri Apr 26 22:18:36 2024 +0800
    
        Style buttons
    
    commit fd5ecf8963c7990dcfd49c92f41f74f8f89e1015
    Author: Jiahe <yi.jiahe00@gmail.com>
    Date:   Fri Apr 26 22:05:14 2024 +0800
    
        Implement card carousel

--- src/StreamStatus.tsx
+++ src/StreamStatus.tsx
@@ -1,27 +1,38 @@
+import { useState } from 'react';
+import './StreamStatus.css';
 import { Stream } from './types.ts';
 import VideoCard, { EmptyVideoCard } from './VideoCard.tsx';
 
 type StreamStatusProps = {
-  stream: Stream | null
+  streams: Array<Stream> | null
 }
 
 function StreamStatus(props: StreamStatusProps) {
-  if (props.stream === null) {
+  const [streamIndex, setStreamIndex] = useState(0);
+
+  if (props.streams === null) {
     return (
       <div>
-        <p style={{marginBottom: '0'}}>Next Stream:</p>
+        <p style={{ marginBottom: '0' }}>Next Stream:</p>
         <EmptyVideoCard />
       </div>
     );
   }
 
-  const startTime = props.stream.ActualStartTime === undefined ? props.stream.ScheduledStartTime : props.stream.ActualStartTime;
+
+  const stream = props.streams[streamIndex];
+
+  const startTime = stream.ActualStartTime === undefined ? stream.ScheduledStartTime : stream.ActualStartTime;
 
   return (
-        <div>
-          <p style={{marginBottom: '0'}}>{Date.now() >= Date.parse(startTime) ? "BAU BAU NAU!! 🐾🩵🩷" : "Next Stream:" }</p>
-          <VideoCard link={`https://www.youtube.com/watch?v=${props.stream.Id}`} thumbnail={props.stream.Thumbnail} title={props.stream.Title} startTime={startTime}></VideoCard>
-        </div>
+    <div className='stream-status'>
+      <p style={{ marginBottom: '0' }}>{Date.now() >= Date.parse(startTime) ? "BAU BAU NAU!! 🐾🩵🩷" : "Next Stream:"}</p>
+      <div className='card-carousel'>
+        <button id='card-carousel-back-button' onClick={() => setStreamIndex(streamIndex - 1)} disabled={streamIndex <= 0}>◀ </button>
+        <VideoCard link={`https://www.youtube.com/watch?v=${stream.Id}`} thumbnail={stream.Thumbnail} title={stream.Title} startTime={startTime}></VideoCard>
+        <button id='card-carousel-forward-button' onClick={() => setStreamIndex(streamIndex + 1)} disabled={streamIndex >= props.streams.length - 1}>▶</button>
+      </div>
+    </div>
   );
 }
 
